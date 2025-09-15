d1047304841cb43984589e667fd03ce822b1ff24
Simrankaur7040 <simrankaur22370@gmail.com>
2025-09-15 20:54:57 +0530

    Add files via upload

---
+++ Level1_String/ArrayIndexDemo.java
@@ -0,0 +1,31 @@
+// 8 Write a program to demonstrate ArrayIndexOutOfBoundsException
+
+package Level1PracticePrograms;
+import java.util.Scanner;
+
+public class ArrayIndexDemo {
+    public static void generateException(String[] names) {
+        System.out.println("Accessing invalid index: " + names[names.length]);
+    }
+    public static void handleException(String[] names) {
+        try {
+            System.out.println("Accessing invalid index: " + names[names.length]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught: " + e);
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException caught: " + e);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        String[] names = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.next();
+        }
+        handleException(names);
+        sc.close();
+    }
+}
